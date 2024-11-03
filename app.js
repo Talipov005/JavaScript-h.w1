37e9f90f5e81a308096b029792dcdcfe31a1da44
Talipov005 <talipovokuu@gmail.com>
2024-11-03 18:41:57 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let bir = prompt("Биринчи тууганым")
+let eki = prompt("Экинчи тууганым")
+let uch = prompt("Учунчу тууганым")
+
+
+console.log(`${bir}  уйдун улуу кызы. ${eki}  улуу агам. ${uch}  кичуу агам.`)
+
+
